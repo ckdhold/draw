9249ec0f83f3b02a824bc5c137cc3c31f394e0fd
ckd <867497106@qq.com>
2026-01-13 11:35:35 +0800

    feat(备案信息): 添加工信部和公安备案链接及样式
    添加备案信息链接功能，支持点击跳转到工信部和公安备案查询网站
    调整页脚样式，包括链接下划线和备案图标样式

--- src/views/Config/index.vue
+++ src/views/Config/index.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { configRoutesChildren } from '../../router'
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { configRoutesChildren } from "../../router";
 
-const router = useRouter()
-const route = useRoute()
+const router = useRouter();
+const route = useRoute();
 
 // 过滤掉重定向路由，只保留有 meta 的菜单项
 const menuList = computed(() => {
-  return configRoutesChildren.filter(item => item.meta)
-})
+  return configRoutesChildren.filter((item) => item.meta);
+});
 
 function skip(path: string) {
-  router.push(path)
+  router.push(path);
 }
+// 跳转到工信部备案查询网站
+const openBeianLink = (type: string) => {
+  if (type === "icp") {
+    window.open("https://beian.miit.gov.cn/", "_blank");
+  } else if (type === "gongan") {
+    window.open("https://beian.mps.gov.cn/#/", "_blank");
+  }
+};
 </script>
 
 <template>
@@ -21,7 +29,11 @@ function skip(path: string) {
     <ul class="pt-14 m-0 mr-3 w-56 min-w-56 menu bg-base-200">
       <li v-for="item in menuList" :key="item.name">
         <a
-          :style="item.name === route.name ? 'background-color:rgba(12,12,12,0.2)' : ''"
+          :style="
+            item.name === route.name
+              ? 'background-color:rgba(12,12,12,0.2)'
+              : ''
+          "
           @click="skip(item.path)"
         >
           <svg-icon v-if="item.meta?.icon" :name="item.meta.icon" />
@@ -31,30 +43,34 @@ function skip(path: string) {
     </ul>
     <router-view class="flex-1 mt-5" />
   </div>
-  <footer class="p-10 rounded footer footer-center bg-base-200 text-base-content">
+  <footer
+    class="p-10 rounded footer footer-center bg-base-200 text-base-content"
+  >
     <aside>
       <div class="flex gap-2 justify-center items-center">
         <p class="p-0 m-0 text-center">
-          Copyright &copy; 2025-2030.
+          Copyright &copy;
           <a
             href="https://www.cked.cn/index.html#/pages/profile/profile"
             target="_blank"
             rel="noopener noreferrer"
-            class="font-normal no-underline text-inherit"
-          >cked.cn</a>.
+            class="font-normal underline text-inherit"
+            >www.cked.cn</a
+          >&nbsp;All Rights Reserved.
           <a
             href="https://www.cked.cn/index.html#/pages/contact/contact"
             target="_blank"
             rel="noopener noreferrer"
-            class="ml-1 font-normal no-underline text-inherit"
-          >意见反馈.</a>
+            class="ml-1 font-normal underline text-inherit"
+            >意见反馈.</a
+          >
           <a
             href="https://www.cked.cn/index.html#/pages/donate/donate"
             target="_blank"
             rel="noopener noreferrer"
-            class="ml-1 font-normal no-underline text-inherit"
-          >赞赏.</a>
-          All Rights Reserved.
+            class="ml-1 font-normal underline text-inherit"
+            >赞赏.</a
+          >
         </p>
         <a
           href="https://github.com/ckdhold/luck#"
@@ -65,8 +81,37 @@ function skip(path: string) {
           <svg-icon name="github" />
         </a>
       </div>
+      <div style="cursor: pointer; display: flex">
+        <div style="cursor: pointer; display: flex">
+          <img
+            class="ga-icon"
+            src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png"
+            style="object-fit: contain"
+          />
+          <span @click="openBeianLink('gongan')" class="beian-link"
+            >浙公网安备33011002018531号</span
+          >
+        </div>
+        <div style="cursor: pointer; display: flex">
+          <img
+            class="ga-icon"
+            src="https://gw.alicdn.com/tfs/TB1GxwdSXXXXXa.aXXXXXXXXXXX-65-70.gif"
+            style="object-fit: contain"
+          />
+          <span @click="openBeianLink('icp')" class="beian-link"
+            >浙ICP备2025183846号-3</span
+          >
+        </div>
+      </div>
     </aside>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.ga-icon {
+  width: 16px;
+  height: 16px;
+  vertical-align: middle;
+  margin: 0 5px;
+}
+</style>
